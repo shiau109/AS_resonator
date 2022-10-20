f34babd7cd0f81865b084d36b1f41e5ef25b00d5
shiau109 <shiau109@gmail.com>
2022-10-21 01:27:16 +0800

    Assignment function finished
    TODO fit tan loss

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ sample_name = "88v1"
 
 #check_configure(sample_name, ["power", "tan_loss"])
 raw_data_fd = f"{sample_name}/raw"
-assignment = pd.read_json(f"{raw_data_fd}/assignment.json")
 
+# Fit each cavity file (mat file) in a sample
 mat_files = check_file_extension( raw_data_fd, "mat")
 file_struc = check_subgroup(mat_files)
-sample_result = []
 for cl, flist in file_struc.items():
     result_list = []
     for fn in flist:
@@ -20,21 +19,34 @@ for cl, flist in file_struc.items():
     cavity_result.Name = cl
     save_power_dep(cavity_result, f"{sample_name}/results/power/{cl}.csv")
     plot_powerQ(cavity_result, f"{sample_name}/results/power")
-    sample_result.append(cavity_result)
 
-plot_multiCav_powerQ(sample_result, sample_name, f"{sample_name}/results/power", assignment, save_mode=True)
 
 power_dep_folder = f"{sample_name}/results/power"
-# Find low power Q
+## After assignment each cavity, get foward analysis
+assignment = pd.read_json(f"{raw_data_fd}/assignment.json")
+
 para_list = []
-for fn in check_file_extension( power_dep_folder, "csv"):
-    paras = find_paras(f"{power_dep_folder}/{fn}.csv", "photons", 1)
-    paras["measurement label"] = fn
+sample_result = []
+
+# for fn in check_file_extension( power_dep_folder, "csv"):
+## Collect assigned cavity
+for cl in assignment["measurement label"].values:
+    file_name = f"{power_dep_folder}/{cl}.csv"
+    cavity_result = pd.read_csv( file_name )
+    cavity_result.Name = cl
+    sample_result.append(cavity_result)
+
+    ## Find low power Q
+    paras = find_paras(file_name, "photons", 0.1)
+    paras["measurement label"] = cl
 
     para_list.append( paras )
+
+
+plot_multiCav_powerQ(sample_result, sample_name, f"{sample_name}/results/power", assignment, save_mode=True)
 lpq_result = pd.concat(para_list)
 
 sample_statistic = pd.merge(lpq_result, assignment, on="measurement label")
-
-plot_Qi_sum(sample_statistic, ("Resonant Frequency","(GHz)",1e9,False), ("Internal Q","",1,True), "Qi_dia_corr_err", sample_name, f"{sample_name}/results/fr_Q" )
-plot_Qi_sum(sample_statistic, ("center linewidth","(um)",1,True), ("Internal Q","",1,True), "Qi_dia_corr_err", sample_name, f"{sample_name}/results/lw_Q" )
+# print(sample_statistic)
+plot_df(sample_statistic, ("Resonant Frequency","(GHz)",1e9), ("Internal Q","",1), "Qi_dia_corr_err", f"{sample_name}/results/fr_Q", log_scale=(False,True) )
+plot_df(sample_statistic, ("center linewidth","(um)",1), ("Internal Q","",1), "Qi_dia_corr_err", f"{sample_name}/results/lw_Q", log_scale=(True,True) )
--- plot_method.py
+++ plot_method.py
@@ -47,17 +47,18 @@ def plot_powerQ(df:pd.DataFrame, output_path, save_mode=True):
 def plot_multiCav_powerQ(dfs:List[pd.DataFrame], sample_name, output_path, assignment:pd.DataFrame, save_mode=True ):
     
     print(assignment['measurement label'])
-    plt.figure(figsize=(16,9),facecolor='white')
+    plt.figure(facecolor='white', figsize=(8,5)) #figsize=(16,9),
     for df in dfs:
         print(f"ploting {df.Name}")
         if df.Name in assignment['measurement label'].values:
             asi = assignment.loc[assignment['measurement label'] == df.Name]
-            print(asi["color"].values[0])
+            a_marker_style = asi["marker_style"].values[0]
             a_color = asi["color"].values[0]
+            a_clw = asi["center linewidth"].values[0]
             x_axis = df[["photons"]].values.flatten()
             Qi = df[["Internal Q"]].values.flatten()
             Qi_err = df[["Qi_dia_corr_err"]].values.flatten()
-            plt.errorbar(x_axis,Qi,yerr=Qi_err, fmt="o", c= a_color,label=df.Name)
+            plt.errorbar(x_axis, Qi, yerr=Qi_err, ms = 5, fmt=a_marker_style, c=a_color, label=f"{df.Name}-{a_clw}um")
     plt.xlabel("Photon Number n",fontsize=16)
     #plt.xticks(fontsize=13)
     plt.ylabel("Qi",fontsize=16)
@@ -71,31 +72,32 @@ def plot_multiCav_powerQ(dfs:List[pd.DataFrame], sample_name, output_path, assig
         plt.savefig(f"{output_path}/{sample_name}_allQi.png")
     plt.close()
 
-def plot_Qi_sum( df, xcol, ycol, yerrcol, sample_name, output_name ):
-
+def plot_df( df:pd.DataFrame, xcol, ycol, yerrcol, output_fn, title="", log_scale=(False,False) ):
+    """
+    Arg:
+    log_scale: two element tuple whose value are bool.
+            First for x axis, Second for y axis,.
+            True for log scale, False for Linear scale.
+    """
 
     if type(xcol) is tuple:
         xcol_name = xcol[0]
         xcol_unit = xcol[1]
         xcol_rescale = xcol[2]
-        xcol_log = xcol[3]
     else:
         xcol_name = xcol
         xcol_unit = f""
         xcol_rescale = 1
-        xcol_log = False
 
     if type(ycol) is tuple:
         ycol_name = ycol[0]
         ycol_unit = ycol[1]
         ycol_rescale = ycol[2]
-        ycol_log = ycol[3]
 
     else:
         ycol_name = ycol
         ycol_unit = f""
         ycol_rescale = 1
-        ycol_log = False
 
 
     x = df[[xcol_name]].values.flatten()/xcol_rescale
@@ -109,11 +111,50 @@ def plot_Qi_sum( df, xcol, ycol, yerrcol, sample_name, output_name ):
     plt.ylabel(f"{ycol_name}{ycol_unit}")#,fontsize=30)
     #plt.yticks(fontsize=27)
     #plt.legend( loc="lower right")#,fontsize=25)
-    plt.title( f"{sample_name}" )#,fontsize=30)
-    if xcol_log : plt.xscale('log')
-    if ycol_log : plt.yscale('log')
+    plt.title( f"{title}" )#,fontsize=30)
+    if log_scale[0] : plt.xscale('log')
+    if log_scale[1] : plt.yscale('log')
 
     #plt.show()
-    plt.savefig(f"{output_name}.png")
+    plt.savefig(f"{output_fn}.png")
     plt.close()
 
+def plot_powerQ(df:pd.DataFrame, output_path, save_mode=True):
+    
+    # condi_1 = (df["Qi_dia_corr_err"] / df["Qi_dia_corr"] < 0.1)
+    # condi_2 = (df["absQc_err"] / df["Qc_dia_corr"] < 0.1)
+    # condi_3 = (df["Ql_err"] / df["Ql"] < 0.1)
+    x_axis = df[["photons"]].values.flatten()
+    Qi = df[["Internal Q"]].values.flatten()
+    Qc = df[["Coupling Q"]].values.flatten()
+    Ql = df[["Loaded Q"]].values.flatten()
+    Qi_err = df[["Qi_dia_corr_err"]].values.flatten()
+    Qc_err = df[["absQc_err"]].values.flatten()
+    Ql_err = df[["Ql_err"]].values.flatten()
+
+    # x_axis = df[(condi_1 | condi_2 | condi_3)]["photons"]
+    # Qi = df[(condi_1 | condi_2 | condi_3)]["Qi_dia_corr"]
+    # Qc = df[(condi_1 | condi_2 | condi_3)]["Qc_dia_corr"]
+    # Ql = df[(condi_1 | condi_2 | condi_3)]["Ql"]
+    
+    # Qi_err = df[(condi_1 | condi_2 | condi_3)]["Qi_dia_corr_err"]
+    # Qc_err = df[(condi_1 | condi_2 | condi_3)]["absQc_err"]
+    # Ql_err = df[(condi_1 | condi_2 | condi_3)]["Ql_err"]
+
+    plt.figure(figsize=(16,9),facecolor='white')
+    plt.errorbar(x_axis,Qi,yerr=Qi_err, fmt="o",label="Qi")
+    plt.errorbar(x_axis,Qc,yerr=Qc_err, fmt="o",label="Qc")
+    plt.errorbar(x_axis,Ql,yerr=Ql_err, fmt="o",label="Ql")
+    plt.xlabel("Photon Number n",fontsize=16)
+    #plt.xticks(fontsize=13)
+    plt.ylabel("Quality factor",fontsize=16)
+    #plt.yticks(fontsize=13)
+    plt.legend()
+    #plt.title( f"{df.Name}",fontsize=16)
+    plt.yscale('log')
+    plt.xscale('log')
+    #plt.show()
+    if save_mode:
+        plt.savefig(f"{output_path}/{df.Name}.png")
+    plt.close()
+    return {"x":x_axis,"y":Qi,"err":Qi_err}
